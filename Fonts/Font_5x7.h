326e002833d57a580dbb588826a74d7587434238
Josimar <josimarpereiraleite@gmail.com>
2021-09-19 07:24:06 -0300

    Add files via upload

---
+++ Fonts/Font_5x7.h
@@ -0,0 +1,27 @@
+/*
+	@File 		
+	@Author		JOSIMAR PEREIRA LEITE
+	@country	Brazil
+	@Date		16/09/21
+	
+
+    Copyright (C) 2021  JOSIMAR PEREIRA LEITE
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+#ifndef FONT_5X7_H_
+#define FONT_5X7_H_
+
+#include <avr/pgmspace.h>
+
+const unsigned char font_5x8[] PROGMEM =
+{
+	5, 8, 1, 32, 0,
+	
+};
+
+
+#endif /* FONT_5X7_H_ */
